e9070d87013f9269777d08ce746d64781d6729dd
Janes Roberto <kadoshmt2@gmail.com>
2023-06-29 09:25:51 -0400

    feat: implements Tags componnet

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -21,9 +21,11 @@ export class HomeComponent implements OnInit {
         this.foods = this.foodService.getAllFoodsBySearchTerm(
           params.searchTerm
         );
-      } else {
-        this.foods = this.foodService.getAll();
-      }
+        } else if (params.tag) {
+          this.foods = this.foodService.getAllFoodsByTag(params.tag);
+        } else {
+          this.foods = this.foodService.getAll();
+        }
     });
   }
 }
