d291a54891e1743d1b7e62d5e84e9f13fdb440f7
Janes Roberto <kadoshmt2@gmail.com>
2023-06-28 21:22:02 -0400

    feat: implements search components

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
@@ -8,25 +8,22 @@ import { Food } from 'src/app/shared/models/Food';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   foods: Food[] = [];
-  route: ActivatedRoute;
-
   constructor(
     private foodService: FoodService,
-    activatedRoute: ActivatedRoute
-  ) {
-    this.route = activatedRoute;
-  }
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      if(params.searchTerm) {
-        this.foods = this.foodService.getAllFoodsBtSearchTerm(params.searchTerm)
-      }else{
+    this.activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm) {
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params.searchTerm
+        );
+      } else {
         this.foods = this.foodService.getAll();
       }
     });
-
   }
 }
