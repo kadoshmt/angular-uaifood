6d95b85c511806602daea9673330e989eb9acada
Janes Roberto <kadoshmt2@gmail.com>
2023-07-02 18:46:20 -0400

    feat: add guard to checkout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { RegisterPageComponent } from './components/pages/register-page/register
 import { LoadingComponent } from './components/partials/loading/loading.component';
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
+import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
+import { MapComponent } from './components/partials/map/map.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +47,8 @@ import { CheckoutPageComponent } from './components/pages/checkout-page/checkout
     RegisterPageComponent,
     LoadingComponent,
     CheckoutPageComponent,
+    OrderItemsListComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
