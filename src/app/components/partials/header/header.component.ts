2971abf500b34bdc2a388c81131c5b1ccfccb73c
Janes Roberto <kadoshmt2@gmail.com>
2023-07-02 11:37:37 -0400

    feat: implements login and logout funtionallity

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +9,29 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-cartQuantity = 0;
-constructor(private cartService: CartService){}
+
+  cartQuantity = 0;
+  user!:User;
+
+  constructor(
+    private cartService: CartService,
+    private userService: UserService
+  ) {}
+
   ngOnInit(): void {
     this.cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
+    });
+    this.userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
     })
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+  get isAuth(){
+    return this.user.token;
   }
 }
