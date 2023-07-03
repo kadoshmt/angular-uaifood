8bb11907a7f443e6b624eeb9cd9afb01c46459ea
Janes Roberto <kadoshmt2@gmail.com>
2023-07-03 10:27:12 -0400

    feat: implements order page

---
+++ src/app/components/pages/order-track-page/order-track-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/shared/models/Order';
+
+@Component({
+  selector: 'app-order-track-page',
+  templateUrl: './order-track-page.component.html',
+  styleUrls: ['./order-track-page.component.css'],
+})
+export class OrderTrackPageComponent implements OnInit {
+  order!: Order;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private orderService: OrderService
+  ) {}
+
+  ngOnInit(): void {
+    const params = this.activatedRoute.snapshot.params;
+    if (!params.orderId) return;
+
+    this.orderService.trackOrderById(params.orderId).subscribe((order) => {
+      this.order = order;
+    });
+  }
+}
