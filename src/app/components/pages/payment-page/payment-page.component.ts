9f4bcfc13e1abae1a03f86b67db73b0569ce14fd
Janes Roberto <kadoshmt2@gmail.com>
2023-07-03 10:26:32 -0400

    feat: implements payment page

---
+++ src/app/components/pages/payment-page/payment-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/shared/models/Order';
+
+@Component({
+  selector: 'app-payment-page',
+  templateUrl: './payment-page.component.html',
+  styleUrls: ['./payment-page.component.css'],
+})
+export class PaymentPageComponent implements OnInit {
+  order: Order = new Order();
+  constructor(private orderService: OrderService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.orderService.getNewOrderForCurrentUser().subscribe({
+      next: (order) => {
+        this.order = order;
+      },
+      error: () => {
+        this.router.navigateByUrl('/chekcout');
+      },
+    });
+  }
+}
