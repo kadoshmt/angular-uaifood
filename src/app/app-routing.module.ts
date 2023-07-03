f97254c6e9a76e95d4a0dfd42c4a654247246e3b
Janes Roberto <kadoshmt2@gmail.com>
2023-07-03 10:27:30 -0400

    fix: localize currency to BRL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
+import { OrderTrackPageComponent } from './components/pages/order-track-page/order-track-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +19,8 @@ const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'checkout', component: CheckoutPageComponent, canActivate:[AuthGuard] },
+  { path: 'payment', component: PaymentPageComponent, canActivate:[AuthGuard] },
+  { path: 'track/:orderId', component: OrderTrackPageComponent, canActivate:[AuthGuard] },
 ];
 
 @NgModule({
